52eaab740d8dc6bc7c56fe544ee46607c9417be1
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-11-30 22:18:30 +0800

    gold armor

--- src/main/java/dev/xkmc/more_wolf_armors/init/MWAItems.java
+++ src/main/java/dev/xkmc/more_wolf_armors/init/MWAItems.java
@@ -8,7 +8,7 @@ import net.minecraft.world.item.*;
 public class MWAItems {
 
 	public static final ItemEntry<Item> TEMPLATE;
-	public static final ItemEntry<WolfArmorItem> IRON;
+	public static final ItemEntry<WolfArmorItem> IRON, GOLD;
 
 	static {
 		MoreWolfArmors.REGISTRATE.defaultCreativeTab(CreativeModeTabs.INGREDIENTS);
@@ -18,6 +18,11 @@ public class MWAItems {
 		IRON = MoreWolfArmors.REGISTRATE.item("iron_wolf_armor", p -> new WolfArmorItem(
 				ArmorMaterials.IRON, false, () -> Items.IRON_INGOT,
 				new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(15))
+		)).tag(MWATagGen.WOLF_ARMORS).register();
+
+		GOLD = MoreWolfArmors.REGISTRATE.item("gold_wolf_armor", p -> new WolfArmorItem(
+				ArmorMaterials.GOLD, false, () -> Items.GOLD_INGOT,
+				new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(7))
 		)).tag(MWATagGen.WOLF_ARMORS).register();
 	}
 
